afc90d0fe6a7fd0c237ae25ae483ebd30ffaa496
Santripas <81268287+santripas@users.noreply.github.com>
2023-07-25 16:18:27 -0300

    Update anatomy.js

--- função/anatomy.js
+++ função/anatomy.js
@@ -6,7 +6,21 @@ function sayHello() {
 sayHello()
 
 function sayHelloTo(name) {
-    console.log('Hello ' + name)
+    console.log(`Hello ${name}!`)
 }
 
 sayHelloTo('Mike')
+
+function returnHi(){
+    return 'Hi!'
+}
+
+var greeting = returnHi()
+console.log(greeting)
+console.log(returnHi())
+
+function returnHiTo(name){
+    return `Hi ${name}!`
+}
+
+console.log(returnHiTo('Sandro'))
